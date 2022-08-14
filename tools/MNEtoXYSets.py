c49f6bb108f9cae2f6763bfce8d1988659667705
Bziomek97 <B.ziomek97@gmail.com>
2022-08-14 12:10:06 +0200

    Init for classical methods

---
+++ tools/MNEtoXYSets.py
@@ -0,0 +1,58 @@
+import mne
+
+def get_label(edf, edf_file):
+    if len(edf_file) == 0:
+        patient_edf_file_name = edf.filenames[0].split('\\')[-1]
+        isSick = patient_edf_file_name.lower().startswith('s')
+    else:
+        patient_edf_file_name = edf_file.split('\\')[-1]
+        isSick = patient_edf_file_name.lower().startswith('s')
+    return int(isSick == True) # 1 - is sick, 0 is healthy
+
+def print_info(epochs_num_per_patient, labels):
+    print('\nEpochs number per patient: ', epochs_num_per_patient)
+    
+    class_0_num = sum(labels) 
+    class_1_num = len(labels)-sum(labels)
+
+    print('\nnegative: ', class_0_num)
+    print('positive: ', class_1_num)
+
+def transform_patients_data_into_X_y_sets(patients_data, info=True):
+    epochs_per_patient = []
+    labels = []
+    
+    epochs_num_per_patient = []
+    for edf in patients_data:
+        epochs = mne.make_fixed_length_epochs(edf, duration=25, preload=True, verbose=False)
+        
+        epochs_per_patient.append(epochs)
+        epochs_num_per_patient.append(len(epochs))
+        
+        label = get_label(edf)
+        labels.extend([label for _ in epochs])
+    
+    epochs = mne.concatenate_epochs(epochs_per_patient)
+    
+    if info:
+        print_info(epochs_num_per_patient, labels)
+
+
+def transform(patients_data, info=False, edf_file=[]):
+    epochs_per_patient = []
+    labels = []
+    
+    for index, edf in enumerate(patients_data):
+        epochs = mne.make_fixed_length_epochs(edf, duration=25, preload=True, verbose=False)
+        
+        epochs_per_patient.append(epochs)
+        
+        label = get_label(edf, edf_file[index])
+        labels.extend([label for _ in epochs])
+    
+    epochs = mne.concatenate_epochs(epochs_per_patient)
+    
+    if info:
+        print_info(len(epochs), labels)
+
+    return (epochs, labels)
---
+++ tools/eegPowerBand.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from mne.time_frequency import psd_welch
+
+def eeg_power_band(epochs):
+    """EEG relative power band feature extraction.
+
+    This function takes an ``mne.Epochs`` object and creates EEG features based
+    on relative power in specific frequency bands that are compatible with
+    scikit-learn.
+
+    Parameters
+    ----------
+    epochs : Epochs
+        The data.
+
+    Returns
+    -------
+    X : numpy array of shape [n_samples, 5]
+        Transformed data.
+    """
+    # specific frequency bands
+    FREQ_BANDS = {"delta": [0.5, 4.5],
+                  "theta": [4.5, 8.5],
+                  "alpha": [8.5, 11.5],
+                  "sigma": [11.5, 15.5],
+                  "beta": [15.5, 30]}
+
+    psds, freqs = psd_welch(epochs, picks='eeg', fmin=0.5, fmax=30.)
+    # Normalize the PSDs
+    psds /= np.sum(psds, axis=-1, keepdims=True)
+
+    X = []
+    for fmin, fmax in FREQ_BANDS.values():
+        psds_band = psds[:, :, (freqs >= fmin) & (freqs < fmax)].mean(axis=-1)
+        X.append(psds_band.reshape(len(psds), -1))
+
+    return np.concatenate(X, axis=1)
